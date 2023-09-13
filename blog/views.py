669152607f58d97358d2019329af5b83f8f3988f
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-13 19:57:33 -0400

    about view created

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django import HttpResponse
+from django.http import HttpResponse
 
 
-def Home(response):
+def home(response):
     return HttpResponse('<h1>Blog Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
