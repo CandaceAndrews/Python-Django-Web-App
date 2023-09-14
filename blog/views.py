6eee84bbf8060570050d5cd93dd9f145f18244ce
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-14 18:13:08 -0400

    dummy data posts for home.html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Leon Kennedy',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 14, 2023'
+    },
+    {
+        'author': 'Claire Redfield',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 13, 2023'
+    },
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
