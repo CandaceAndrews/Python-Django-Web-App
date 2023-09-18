4cb703fb3a45ec69e12d6f0949ddb5677a835c54
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-18 18:15:57 -0400

    edited Post template date display, added Post model to admin

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Leon Kennedy',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 14, 2023'
-    },
-    {
-        'author': 'Claire Redfield',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 13, 2023'
-    },
-]
-
 
 def home(request):
     context = {'posts': Post.objects.all()}
