02858433fba41b77f187f37289449852dc6cde32
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-19 20:10:04 -0400

    editing view to recieve post request data and create new user with it.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
@@ -7,6 +8,8 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
