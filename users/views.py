e443aac84b71f727fd1c13e6975a5c2bd654743b
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-26 17:52:33 -0400

    Form for User to update email or username

--- users/views.py
+++ users/views.py
@@ -26,6 +26,9 @@ def profile(request):
             request.POST,
             request.FILES,
             instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
