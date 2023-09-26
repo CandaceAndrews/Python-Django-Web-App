d43a762269f141c7a0a943d32fcda0fcdeaf8ff4
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-25 21:03:20 -0400

    Form for User to update email or username

--- users/views.py
+++ users/views.py
@@ -20,11 +20,11 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
-        'u_from': u_form,
+        'u_form': u_form,
         'p_form': p_form,
     }
 
